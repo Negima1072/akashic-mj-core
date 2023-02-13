cde90ecc50cc8e7179a360622f4c0c3e349b97e5
Negima1072 <ngm1072@gmail.com>
2023-02-14 01:30:59 +0900

    add: index

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @negima1072/akashic-mj-core v0.1.0
+ *
+ * Copyright(C) 2023 Negima1072
+ * Released under the MIT license
+ * https://github.com/Negima1072/akashic-mj-core/main/LICENSE
+ */
+
+export { Rule, rule } from "./rule";
+export { Shoupai } from "./shoupai";
+export { Shan } from "./shan";
